90aa84bb75c2306bea4da205e999952dd39301cc
Oliver Lanz <oliver.lanz@gigglingbits.com>
2026-01-16 10:57:44 +0000

    All visual effects not show based on declarative properties

--- src/main/kotlin/ch/oliverlanz/memento/application/visualization/effects/LorestonePlacementEffect.kt
+++ src/main/kotlin/ch/oliverlanz/memento/application/visualization/effects/LorestonePlacementEffect.kt
@@ -5,31 +5,29 @@ import ch.oliverlanz.memento.application.visualization.samplers.StoneBlockSample
 import ch.oliverlanz.memento.domain.stones.LorestoneView
 import net.minecraft.particle.ParticleTypes
 
-class LorestonePlacementEffect(
-    private val stone: LorestoneView
-) : EffectBase() {
+class LorestonePlacementEffect(private val stone: LorestoneView) : EffectBase() {
 
     override fun onConfigure(profile: EffectProfile) {
-    profile.lifetimeGameHours = 1.0 // 1000 game ticks
+        profile.lifetimeGameHours = 1.0 // 1000 game ticks
 
-    // Subtle initial burst to make placements perceptible.
-    profile.burstDurationGameHours = 0.05 // ~50 game ticks (~2.5s)
-    profile.burstMultiplier = 2.0
+        // Subtle initial burst to make placements perceptible.
+        profile.burstDurationGameHours = 0.05 // ~50 game ticks (~2.5s)
+        profile.burstMultiplier = 2.0
 
-    profile.anchorSampler = StoneBlockSampler(stone)
-    profile.anchorSystem = StoneParticleSystems.anchorFor(stone)
-    profile.anchorRatePerGameHour = 150.0 // 0.15 per server tick
+        profile.anchorSampler = StoneBlockSampler(stone)
+        profile.anchorSystem = StoneParticleSystems.anchorFor(stone)
+        profile.anchorRatePerGameHour = 150.0 // 0.15 per server tick
 
-    profile.surfaceSampler = SingleChunkSurfaceSampler(stone)
-    profile.surfaceDustSystem = StoneParticleSystems.surfaceDustFor(stone)
-    profile.surfaceDustRatePerGameHour = 150.0 // 0.15 per server tick
-    profile.surfaceDustSubsetSize = 32
-    profile.surfaceDustSubsetSeed = stone.position.asLong()
+        profile.surfaceSampler = SingleChunkSurfaceSampler(stone)
+        profile.surfaceDustSystem = StoneParticleSystems.surfaceDustFor(stone)
+        profile.surfaceDustRatePerGameHour = 150.0 // 0.15 per server tick
+        profile.surfaceDustSubsetSize = 32
+        profile.surfaceDustSubsetSeed = stone.position.asLong()
 
-    profile.surfacePresenceSystem = StoneParticleSystems.surfacePresenceFor(ParticleTypes.ENCHANT)
-    profile.surfacePresenceRatePerGameHour = 50.0 // 0.05 per server tick
+        profile.surfacePresenceSystem =
+                StoneParticleSystems.surfacePresenceFor(ParticleTypes.ENCHANT)
+        profile.surfacePresenceRatePerGameHour = 50.0 // 0.05 per server tick
 
-    profile.yOffsetSpread = 0..3
+        profile.yOffsetSpread = 0..3
     }
 }
-
