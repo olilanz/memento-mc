7ef6c8565184bc479d86b01852a92fa328d30f7e
Oliver Lanz <oliver.lanz@gigglingbits.com>
2026-02-14 20:38:27 +0000

    Further boost of visual effects.

--- src/main/kotlin/ch/oliverlanz/memento/application/visualization/effects/StoneInspectionEffect.kt
+++ src/main/kotlin/ch/oliverlanz/memento/application/visualization/effects/StoneInspectionEffect.kt
@@ -11,12 +11,12 @@ class StoneInspectionEffect(stone: StoneView) : EffectBase(stone) {
     override fun onConfigure(profile: EffectProfile) {
         profile.lifetime = GameHours(0.15)  // Inspection: short-lived and high signal.
 
-        profile.stoneChunk.emissionsPerGameHour = 440
+        profile.stoneChunk.emissionsPerGameHour = 1760
         profile.stoneChunk.verticalSpan = 0..3
         profile.stoneChunk.system = endRodSystem(12, 0.45, 0.15, 0.45, 0.01, 1.0)
 
         profile.influenceArea.sampler = InfluenceAreaSurfaceSampler(stone)
-        profile.influenceArea.emissionsPerGameHour = 1000
+        profile.influenceArea.emissionsPerGameHour = 4000
         profile.influenceArea.verticalSpan = 0..2
         profile.influenceArea.materialization = SamplerMaterializationConfig(detail = 0.35)
         profile.influenceArea.dominanceMode = EffectProfile.DominanceMode.COLOR_BY_DOMINANT
@@ -25,7 +25,7 @@ class StoneInspectionEffect(stone: StoneView) : EffectBase(stone) {
         profile.influenceArea.dominantWitherSystem = soulFireFlameSystem(10, 0.375, 0.12, 0.375, 0.01, 1.0)
 
         profile.influenceOutline.sampler = InfluenceOutlineSurfaceSampler(stone, thicknessBlocks = 4)
-        profile.influenceOutline.emissionsPerGameHour = 520
+        profile.influenceOutline.emissionsPerGameHour = 2080
         profile.influenceOutline.verticalSpan = 0..2
         profile.influenceOutline.materialization = SamplerMaterializationConfig(detail = 0.50)
         profile.influenceOutline.dominanceMode = EffectProfile.DominanceMode.COLOR_BY_DOMINANT
